97fd1a417786f2278628458e943112deb14757f2
Mohammad Jafar Jafar <jafarkashify@gmail.com>
2025-01-18 04:26:42 +0330

    handleClient and saveData method added

--- CollaboCanvas/src/main/java/com/saberi/collabocanvas/Server.java
+++ CollaboCanvas/src/main/java/com/saberi/collabocanvas/Server.java
@@ -50,5 +50,62 @@ public class Server {
      * @param clientSocket The socket connected to the client.
      * @param socket       The socket used for communication.
      */
-    
+    public static void handleClient(Socket clientSocket, Socket socket) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+            String jsonString = in.readLine();
+            System.out.println("Recieved: " + jsonString);
+            JSONParser parser = new JSONParser();
+            JSONObject obj = (JSONObject) parser.parse(jsonString);
+
+            String type = (String) obj.get("Type");
+            if (type.equals("REGISTER")) {
+                String userName = (String) obj.get("Username");
+                String email = (String) obj.get("Email");
+                String password = (String) obj.get("Password");
+                System.out.println(" type " + type);
+                System.out.println(" user name" + userName);
+                System.out.println("email  " + email);
+                System.out.println("password " + password);
+            }
+            else {
+                String userName = (String) obj.get("Username");
+                String password = (String) obj.get("Password");
+                System.out.println(" type " + type);
+                System.out.println(" user name" + userName);
+                System.out.println("password " + password);
+            }
+
+
+            int result = check(obj);
+            if ((result == 0) && (obj.get("Type").equals("REGISTER"))) {
+                saveDataToFile(obj); // save the data in json file
+            }
+            System.out.println("result = " + result);
+            out.writeUTF("" + result);
+            //saveToFile(jsonString); // save data to json file
+        } catch (ParseException parseException) {
+            parseException.printStackTrace();
+        }
+        catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+    }
+    /**
+     * Saves a JSON object to the data file.
+     *
+     * @param jsonObject The JSON object to save.
+     */
+    public static void saveDataToFile(JSONObject jsonObject) {
+        try {
+            FileWriter fileWriter = new FileWriter("DataFile.json", true);
+            fileWriter.write(jsonObject.toJSONString());
+            fileWriter.write(System.lineSeparator());
+            fileWriter.close();
+        } catch (IOException ioException)  {
+            ioException.printStackTrace();
+        }
+    }
 }
